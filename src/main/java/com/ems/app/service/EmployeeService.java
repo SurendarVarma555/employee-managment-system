b60813a585468a4842f5cbb7032dbf26099f1244
Surendar <batthulasurendar@gmail.com>
2023-04-15 14:08:07 +0530

    Implemented Java 8 feature

--- src/main/java/com/ems/app/service/EmployeeService.java
+++ src/main/java/com/ems/app/service/EmployeeService.java
@@ -14,4 +14,5 @@ public interface EmployeeService {
 
 
     Employee updateEmployee (Employee employee, Integer id);
+
 }
--- src/main/java/com/ems/app/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/ems/app/service/impl/EmployeeServiceImpl.java
@@ -28,21 +28,30 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Override
     public Employee getEmployeeById(Integer id){
-        Optional<Employee> employee = employeeRepository.findById(id);
-        if(employee.isPresent()){
-            return employee.get();
-        }else {
-            throw new ResourceNotFoundException("Employee","id",id);
-        }
+//        Optional<Employee> employee = employeeRepository.findById(id);
+//        if(employee.isPresent()){
+//            return employee.get();
+//        }else {
+//            throw new ResourceNotFoundException("Employee","id",id);
+//        }
+        return  employeeRepository.findById(id).orElseThrow(
+                ()-> new ResourceNotFoundException("Employee","id",id));
     }
 
     @Override
     public Employee updateEmployee (Employee employee, Integer id){
+
+        // first we need to check whether employee with given id is exist or not
+
         Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
                                                             ()-> new ResourceNotFoundException("Employee","id",id));
+
+        // We have to set/update the existing employee by setting the values which came from client end i.e.,, Employee employee
         existingEmployee.setFirstName(employee.getFirstName());
         existingEmployee.setLastName(employee.getLastName());
         existingEmployee.setEmail(employee.getEmail());
+
+        // Saving updated details in at server side
         employeeRepository.save(existingEmployee);
         return existingEmployee;
     }
